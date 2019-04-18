9a1ab1a0c2aebb4ce875b004ffd0c3db2dc085fc
Corey <hutcheson.coreyJ@gmail.com>
2019-04-18 12:21:14 -0500

    created event templated page query

--- src/templates/event.jsx
+++ src/templates/event.jsx
@@ -1,12 +1,40 @@
 import React from "react";
+import { graphql } from "gatsby";
 
 import PageWrapper from "src/components/page-wrapper";
 // import { useWordpressEvents } from "src/utils/js/custom-hooks/useWordpressEvents";
 
-const EventTemplate = ({ pageContext }) => {
-  console.log(pageContext);
+const EventTemplate = ({ data }) => {
+  const {
+    wordpressWpEvent: { title, acf },
+  } = data;
 
-  return <PageWrapper>Event</PageWrapper>;
+  console.log(acf.event_name);
+
+  return (
+    <PageWrapper>
+      <div>{title}</div>
+      <div>{`Event_Date: ${acf.event_date}`}</div>
+    </PageWrapper>
+  );
 };
+
+export const query = graphql`
+  query SingleWordpressEvent($id: String!) {
+    wordpressWpEvent(id: { eq: $id }) {
+      id
+      title
+      acf {
+        event_name
+        event_date
+        featured_event
+        use_card_details
+        use_custom_images
+        card_details
+        extra_card_details
+      }
+    }
+  }
+`;
 
 export default EventTemplate;
