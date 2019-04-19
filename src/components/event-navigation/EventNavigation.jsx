a3c88119d3d8a326a061914fa541464ecdfd00fb
Corey <hutcheson.coreyJ@gmail.com>
2019-04-19 15:47:50 -0500

    styled EventNavButton and EventNavigation components

--- src/components/event-navigation/EventNavigation.jsx
+++ src/components/event-navigation/EventNavigation.jsx
@@ -5,29 +5,27 @@ import { faBackward, faForward } from "@fortawesome/free-solid-svg-icons";
 import EventNavButton from "./event-nav-button";
 import "./style.scss";
 
-const EventNavigation = ({ prevEvent, nextEvent }) => {
-  return (
-    <div className="event-navigation">
-      {prevEvent ? (
-        <EventNavButton
-          direction="left"
-          link={prevEvent}
-          icon={faBackward}
-          text="Prev"
-        />
-      ) : null}
+const EventNavigation = ({ prevEvent, nextEvent }) => (
+  <div className="event-navigation">
+    {prevEvent ? (
+      <EventNavButton
+        direction="left"
+        link={prevEvent}
+        icon={faBackward}
+        text="Prev"
+      />
+    ) : null}
 
-      {nextEvent ? (
-        <EventNavButton
-          direction="right"
-          link={nextEvent}
-          icon={faForward}
-          text="Next"
-        />
-      ) : null}
-    </div>
-  );
-};
+    {nextEvent ? (
+      <EventNavButton
+        direction="right"
+        link={nextEvent}
+        icon={faForward}
+        text="Next"
+      />
+    ) : null}
+  </div>
+);
 
 EventNavigation.propTypes = {
   prevEvent: PropTypes.string,
