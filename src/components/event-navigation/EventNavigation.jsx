c6f6e9977843b76f47039a0cc9abc43011d20596
Corey <hutcheson.coreyJ@gmail.com>
2019-04-19 15:30:22 -0500

    created EventNavigation component to add Prev and Next buttons to event pages

---
+++ src/components/event-navigation/EventNavigation.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { faBackward, faForward } from "@fortawesome/free-solid-svg-icons";
+
+import EventNavButton from "./event-nav-button";
+import "./style.scss";
+
+const EventNavigation = ({ prevEvent, nextEvent }) => {
+  return (
+    <div className="event-navigation">
+      {prevEvent ? (
+        <EventNavButton
+          direction="left"
+          link={prevEvent}
+          icon={faBackward}
+          text="Prev"
+        />
+      ) : null}
+
+      {nextEvent ? (
+        <EventNavButton
+          direction="right"
+          link={nextEvent}
+          icon={faForward}
+          text="Next"
+        />
+      ) : null}
+    </div>
+  );
+};
+
+EventNavigation.propTypes = {
+  prevEvent: PropTypes.string,
+  nextEvent: PropTypes.string,
+};
+
+export default EventNavigation;
