16a0037a255da20192de531e1abe71ce34d91460
Corey Hutcheson <hutcheson.coreyj@gmail.com>
2019-04-20 12:48:37 -0500

    EventBanner now takes title and link properties | basic styles applied

--- src/components/event-banner/EventBanner.jsx
+++ src/components/event-banner/EventBanner.jsx
@@ -1,19 +1,25 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faStar } from "@fortawesome/free-brands-svg-icons";
+import PropTypes from "prop-types";
 import { Link } from "gatsby";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
 
 import "./style.scss";
 
-const EventBanner = () => (
+const EventBanner = ({ title, slug }) => (
   <div className="featured-notification">
     <FontAwesomeIcon icon={faStar} size="1x" />
-    <Link to="" className="featured-notification__link">
+    <Link to={`/event/${slug}`} className="featured-notification__link">
       <span>Featured:</span>
-      <span>August 17, 2019</span>
+      <span className="featured-notification__title">{title}</span>
     </Link>
     <FontAwesomeIcon icon={faStar} size="1x" />
   </div>
 );
+
+EventBanner.propTypes = {
+  title: PropTypes.string.isRequired,
+  slug: PropTypes.string.isRequired,
+};
 
 export default EventBanner;
