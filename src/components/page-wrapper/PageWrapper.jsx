3dc115238d5668c2dbb84d9af12647ae7763cf1e
Corey Hutcheson <hutcheson.coreyj@gmail.com>
2019-04-20 12:59:09 -0500

    PageWrapper componennt now queries for featured events and displays them if applicable

--- src/components/page-wrapper/PageWrapper.jsx
+++ src/components/page-wrapper/PageWrapper.jsx
@@ -3,16 +3,31 @@ import PropTypes from "prop-types";
 
 import NavBar from "src/components/nav-bar";
 import Footer from "src/components/footer";
+import EventBanner from "src/components/event-banner";
 
-const PageWrapper = ({ children }) => (
-  <>
-    <header id="masthead" className="site-header">
-      <NavBar />
-    </header>
-    <main>{children}</main>
-    <Footer />
-  </>
-);
+import { useFeaturedEvents } from "src/utils/js/custom-hooks/useFeaturedEvents";
+
+const PageWrapper = ({ children }) => {
+  const featuredEvents = useFeaturedEvents();
+  const banners = featuredEvents.map(({ node }) => (
+    <EventBanner
+      title={node.title}
+      slug={node.slug}
+      key={node.fields.timestamp}
+    />
+  ));
+
+  return (
+    <>
+      <header id="masthead" className="site-header">
+        <NavBar />
+      </header>
+      {banners}
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+};
 
 PageWrapper.propTypes = {
   children: PropTypes.node.isRequired,
