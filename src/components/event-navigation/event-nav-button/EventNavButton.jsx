3a939ca9a62270d19ee646692ad25702288c6ea1
Corey <hutcheson.coreyJ@gmail.com>
2019-04-19 23:01:59 -0500

    Switched from <a> element to <Link> element in EventNavButton component

--- src/components/event-navigation/event-nav-button/EventNavButton.jsx
+++ src/components/event-navigation/event-nav-button/EventNavButton.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "gatsby";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import PropTypes from "prop-types";
 
@@ -7,13 +8,13 @@ import "./style.scss";
 const EventNavButton = ({ direction, link, icon, text }) => {
   return (
     <div className={`event-navigation__${direction}-link`}>
-      <a href={`/event/${link}`} title={link}>
+      <Link to={`/event/${link}`} title={link}>
         <div className="event-navigation__button">
           {direction === "right" ? <span>{text}</span> : null}
           <FontAwesomeIcon icon={icon} size="1x" />
           {direction === "left" ? <span>{text}</span> : null}
         </div>
-      </a>
+      </Link>
     </div>
   );
 };
