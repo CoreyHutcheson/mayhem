1f03c3102e049881c7fbd0457792fb9f2e5c9e51
Corey <hutcheson.coreyJ@gmail.com>
2019-06-08 22:01:14 -0500

    Create basic events.jsx page layout
    Base layout and style of events page has been started.

--- src/pages/events.jsx
+++ src/pages/events.jsx
@@ -1,11 +1,34 @@
 import React from "react";
+import Img from "gatsby-image";
 
 import PageWrapper from "src/components/page-wrapper";
+import { useUpcomingEvents } from "src/utils/js/custom-hooks/useUpcomingEvents";
 
-const EventsPage = () => (
-  <PageWrapper>
-    <div>Events Page</div>
-  </PageWrapper>
-);
+import "src/utils/styles/page-styles/events.scss";
+
+const EventsPage = () => {
+  const upcomingEvents = useUpcomingEvents();
+  console.log(upcomingEvents);
+
+  return (
+    <PageWrapper>
+      <div className="event-page__wrapper">
+        {upcomingEvents.map(({ node }) => (
+          <a href={`/events/${node.slug}`} className="event-page__event">
+            <div className="event-page__title" key={node.id}>
+              {node.title}
+            </div>
+            <Img
+              className="event-page__image"
+              key={node.id}
+              fluid={node.acf.event_image.localFile.childImageSharp.fluid}
+              alt={node.title}
+            />
+          </a>
+        ))}
+      </div>
+    </PageWrapper>
+  );
+};
 
 export default EventsPage;
