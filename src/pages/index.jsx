d76d5b3bd3fc51b74ced39d878a7a1fce4ca682c
Corey <hutcheson.coreyJ@gmail.com>
2019-06-09 02:05:12 -0500

    Create Index page hero image content
    Queries the wordpress Index page for the featured image, then
    used gatsby-background-image to display the image, and corresponding
    events link button.

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,45 @@
 import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import BackgroundImage from "gatsby-background-image";
 
 import PageWrapper from "src/components/page-wrapper";
 
 import "src/utils/styles/page-styles/index.scss";
 
 const IndexPage = () => {
+  const landingPageData = useStaticQuery(graphql`
+    query LandingPage {
+      wordpressPage(title: { eq: "Index" }) {
+        featured_media {
+          id
+          localFile {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+      }
+    }
+  `);
+
   return (
     <PageWrapper>
-      <div>Index Page</div>
+      <div className="index-page">
+        <BackgroundImage
+          Tag="div"
+          className="index-page__image"
+          fluid={
+            landingPageData.wordpressPage.featured_media.localFile
+              .childImageSharp.fluid
+          }
+        />
+
+        <a href="/events/" className="index-page__button">
+          Events
+        </a>
+      </div>
     </PageWrapper>
   );
 };
