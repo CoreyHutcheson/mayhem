3e9ed0f4b89b5ccf6240d1c7ad7418a69e9ec174
Corey <hutcheson.coreyJ@gmail.com>
2019-06-08 23:33:14 -0500

    Create basic about.jsx page layout
    Queries wordpress about page. Basic styling applied.

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,11 +1,36 @@
 import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import { Helmet } from "react-helmet";
 
 import PageWrapper from "src/components/page-wrapper";
+import "src/utils/styles/page-styles/about.scss";
 
-const AboutPage = () => (
-  <PageWrapper>
-    <div>About Page</div>
-  </PageWrapper>
-);
+const AboutPage = () => {
+  const aboutData = useStaticQuery(graphql`
+    query AboutPage {
+      wordpressPage(title: { eq: "About" }) {
+        id
+        title
+        content
+      }
+    }
+  `).wordpressPage;
+
+  return (
+    <PageWrapper>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>About Mayhem</title>
+      </Helmet>
+
+      <div className="about-page__title">About Mayhem Wrestling</div>
+
+      <div
+        className="about-page__content"
+        dangerouslySetInnerHTML={{ __html: aboutData.content }}
+      />
+    </PageWrapper>
+  );
+};
 
 export default AboutPage;
