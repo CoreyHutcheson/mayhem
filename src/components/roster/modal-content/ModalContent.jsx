578d80a6509a10ede83d5a211e55d313642e363b
Corey Hutcheson <hutcheson.coreyj@gmail.com>
2019-06-10 13:53:20 -0500

    Add and style content for ModalContent

--- src/components/roster/modal-content/ModalContent.jsx
+++ src/components/roster/modal-content/ModalContent.jsx
@@ -1,5 +1,8 @@
 import React from "react";
+import Img from "gatsby-image";
 import PropTypes from "prop-types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGlobe, faImages, faTimes } from "@fortawesome/free-solid-svg-icons";
 
 import "./style.scss";
 
@@ -14,7 +17,68 @@ const ModalContent = ({
   gallery,
   website,
   handleModalClose,
-}) => <div>{name}</div>;
+}) => (
+  <div className="modal-container">
+    <div className="modal-container__image">
+      <Img fluid={image} alt={name} />
+    </div>
+
+    <div className="modal-container__info">
+      <div className="modal-container__name">{name}</div>
+
+      {nickname && <div className="modal-container__nickname">{nickname}</div>}
+
+      {location && (
+        <div className="modal-container__location">
+          <span>Hometown:</span>
+          {location}
+        </div>
+      )}
+
+      {quick_fact && (
+        <div className="modal-container__quick-fact">
+          <span>Facts: </span>
+          {quick_fact}
+        </div>
+      )}
+
+      {description && (
+        <div className="modal-container__description">
+          <span>Description:</span>
+          {description}
+        </div>
+      )}
+
+      {champion && <div className="modal-container__champion">Belt</div>}
+
+      {(gallery || website) && (
+        <div className="modal-container__links">
+          {gallery && (
+            <FontAwesomeIcon
+              className="modal-container__gallery"
+              icon={faImages}
+            />
+          )}
+          {website && (
+            <a href={website}>
+              <FontAwesomeIcon
+                className="modal-container__website"
+                icon={faGlobe}
+              />
+            </a>
+          )}
+        </div>
+      )}
+    </div>
+
+    <FontAwesomeIcon
+      className="modal-container__close-button"
+      onClick={handleModalClose}
+      icon={faTimes}
+      size="2x"
+    />
+  </div>
+);
 
 ModalContent.propTypes = {
   title: PropTypes.string,
